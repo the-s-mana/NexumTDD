7c27b2ca6570734a9d59cd334e44a9dacce7185f
tee <pootared@perfenterprise.com>
2025-10-17 13:29:23 +0700

    .

--- Nexum.Server/Program.cs
+++ Nexum.Server/Program.cs
@@ -6,6 +6,7 @@ using Nexum.Server.Services;
 using Nexum.Server.Services.Penalty;
 using Nexum.Server.Services.test;
 using SurrealDb.Net;
+using static Nexum.Server.Data.IDbProviderFactory;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,7 +35,10 @@ builder.Services.AddScoped<IPenaltyPolicies, PenaltyPolicies>();
 builder.Services.AddScoped<IFixedPenalty, FixedPenalty>();
 
 builder.Services.AddScoped<ISurrealDbProvider<Book>, SurrealDbProvider<Book>>();
-builder.Services.AddScoped<IBookService, BookService>();
+var surrealDbConfig = builder.Configuration.GetSection("SurrealDbSettings");
+builder.Services.AddSurreal(x => x.FromConnectionString(surrealDbConfig["ConnectionString"]), ServiceLifetime.Scoped);
+builder.Services.AddTransient<IBookService, BookService>();
+builder.Services.AddScoped<SurrealDbProviderFactoryBase, SurrealDbProviderFactory>();
 
 
 var app = builder.Build();
