2a6676fd5c637889423e03a0b338e1766c65e475
tee <pootared@perfenterprise.com>
2025-10-17 11:22:55 +0700

    update Surreal

---
+++ Nexum.Server/Data/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using SurrealDb.Net.Models;
+
+namespace Nexum.Server.Data.Models
+{
+    public class Book : Record
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishYear { get; set; }
+
+        // Constructor ที่ไม่มี Id สำหรับสร้าง Object ใหม่
+        public Book(string title, string author, int publishYear)
+        {
+            Title = title;
+            Author = author;
+            PublishYear = publishYear;
+        }
+    }
+}
---
+++ Nexum.Server/Services/test/BookService.cs
@@ -0,0 +1,22 @@
+﻿using Nexum.Server.Data.Models;
+
+namespace Nexum.Server.Services.test
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+        Task<Book> CreateBookAsync(Book book);
+    }
+    public class BookService : IBookService
+    {
+        public Task<Book> CreateBookAsync(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> GetAllBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
